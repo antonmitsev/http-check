4ea7d565587d73e1301f4c7e2cfff5a5c88aa466
unknown <me@tonymitsev.com>
2018-12-31 13:21:21 +0200

    feat: httpCheck()
    feat: Gets reachable URL
    feat: Gets unreachable URL
    feat: Gets timeout (TODO: is it necessary, how to test it)
    feat: Some helpers and unit tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,24 @@
 const expect = require('chai').expect;
-const httpCheck = require('../index.js');
+const checker = require('../index.js');
 
-describe('httpCheck()', function () {
-  it('should make http request', function () {
+describe('checker.httpCheck()', function () {
+  it('should make http request and report status >= 0 (site reachable)', function (done) {
     // 1. ARRANGE
-    const url = 'http://localost/';
-    const response = 'pong';
-
+    const url = 'https://www.google.com';
     // 2. ACT
-    var check = httpCheck(url);
-
-    // 3. ASSERT
-    expect(check).to.be.equal(response);
+    checker.httpCheck(url).once('requestEnd', (status) => {
+      // 3. ASSERT
+      expect(status.statusCode).to.be.greaterThan(-1);
+      done();
+    });
   });
+
+  it('should make http request and report status < 0 (site unreachable)', function (done) {
+    const url = 'http://d.d/';
+    checker.httpCheck(url).once('requestEnd', (status) => {
+      expect(status.statusCode).to.be.lessThan(0);
+      done();
+    });
+  });
+
 });
